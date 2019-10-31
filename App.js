99d9cf849ddae9ef6b7679f1b4b2a2cb0b3c96d7
Jack Cornish <jack.cornish@bradbourne.digital>
2019-10-31 16:29:20 +0000

    TouchableOpacity

--- App.js
+++ App.js
@@ -1,19 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Homepage</Text>
+    <View>
+      <View style={styles.header}>
+       <Text>Pick your workout.</Text>
+      </View>
+      <View >
+        <TouchableOpacity style={styles.list}>
+          <Text style={styles.text}>
+            Push Pull Legs
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View >
+        <TouchableOpacity style={styles.list}>
+          <Text style={styles.text}>
+            Splits
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View >
+        <TouchableOpacity style={styles.list}>
+          <Text style={styles.text}>
+            Muscle Specific Workouts
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#eee',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+
   },
-});
+  header: {
+    alignItems: "center",
+    paddingVertical: '20%'
+    
+  },
+  list: {
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    marginVertical: 30,
+    minHeight: 50,
+    backgroundColor: "#cc0000",
+  },
+  text: {
+    color: 'white',
+    fontSize: 18
+
+  }
+})
+
